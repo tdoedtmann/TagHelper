e6c6b4e3da7ff0c0c10d99954ff6d6d8a62f9779
Timo Strotmann <strotmann@saltation.de>
2009-10-02 14:16:37 +0200

    Ausgabe/template überarbeitet.

--- demo/includeExampleFiles.php
+++ demo/includeExampleFiles.php
@@ -13,16 +13,18 @@
 				?>
 				<div class="post">
 					<div class="title">
-						<h2><a href="#"><span class="pointer toggle" id="toggleTabBox_<?php echo $index?>" onclick="toogleBox(this, 'boxTabContent_<?php echo $index?>');">[+] <?php echo $title?></span></a></h2>
+						<h2><a href="#"><span class="pointer toggle" id="toggleTabBox_<?php echo $index?>" onclick="toogleBox(this, 'boxTabContent_<?php echo $index?>');"><?php echo (($display=='block') ? '[&minus;]' : '[+]')?> <?php echo $title?></span></a></h2>
 						<p>10.11.08</p>
 					</div>
 					<div class="entry">
+					
 						<div id="boxTabContent_<?php echo $index?>" style="display: <?php echo $display ?>; border: 1px solid black;">
-
 							<div class="tabs">
 								<ul class="tabMenu">
 									<li class="tabMenuItem"><a href="#tabContent_1_<?php echo $index?>">Example</a></li>
 									<li class="tabMenuItem"><a href="#tabContent_2_<?php echo $index?>">Code</a></li>
+									<li class="tabMenuItem"><a href="#tabContent_3_<?php echo $index?>">POST</a></li>
+									<li class="tabMenuItem"><a href="#tabContent_4_<?php echo $index?>">GET</a></li>
 								</ul>
 								<br style="clear: both;" />
 								
@@ -38,6 +40,23 @@
 										</pre>
 									</div>
 								</div>
+								
+								<div id="tabContent_3_<?php echo $index?>">
+									<div id="postTabConent_<?php echo $index?>" style="overflow:auto;">
+										<pre>
+<?php echo var_export($_POST,1); ?>
+										</pre>
+									</div>
+								</div>
+								
+								<div id="tabContent_4_<?php echo $index?>">
+									<div id="getTabConent_<?php echo $index?>" style="overflow:auto;">
+										<pre>
+<?php echo var_export($_GET,1); ?>
+										</pre>
+									</div>
+								</div>
+								
 							</div>
 							<br style="clear: both;" />
 							
